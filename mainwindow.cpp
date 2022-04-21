7c0ba04758a633002c6d2c8036ffa6dc1171f5c2
QuinMeow <nicopoiduang5228@gmail.com>
2022-04-21 17:51:09 +0800

    UI界面完成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QDoubleValidator *X_Validator = new QDoubleValidator(0,10000000,4);
+    QDoubleValidator *Y_Validator = new QDoubleValidator(-500000,500000,4);
+    ui->Coo_X->setValidator(X_Validator);
+    ui->Coo_Y->setValidator(Y_Validator);
+
 }
 
 MainWindow::~MainWindow()
